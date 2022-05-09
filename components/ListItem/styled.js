d6526b83c5e5070cb3c37c622ad57840abcdad86
Deepak Negi <st.deepak15@gmail.com>
2022-05-10 00:21:49 +0530

    fix: styling issue with listItem in mobile-screen

--- components/ListItem/styled.js
+++ components/ListItem/styled.js
@@ -2,12 +2,12 @@ import styled from "styled-components";
 
 export const StyledWrapper = styled.div`
   min-height: 100px;
-  min-width: 680px;
   width: 100%;
   padding: 8px;
   background: #f8f8f8;
   border-radius: 5px;
   margin-bottom: 1rem;
+
   .post__title {
     font-size: 1.2rem;
     font-weight: bold;
